4044053ab1d84cc3cf425f57924a486716859eae
Felix Wieland <mail@felixwie.com>
2024-10-25 10:08:55 +0200

    feat: add http server factory

--- http/server.go
+++ http/server.go
@@ -9,6 +9,16 @@ import (
 	"go.uber.org/fx"
 )
 
+func WithHttpFactory(addr string) fx.Option {
+	return fx.Provide(
+		newHttpServerFactory(addr),
+		fx.Annotate(
+			newServeMux,
+			fx.ParamTags(`group:"routes"`),
+		),
+	)
+}
+
 var WithHttp = fx.Provide(
 	newHttpServer,
 	fx.Annotate(
@@ -16,6 +26,31 @@ var WithHttp = fx.Provide(
 		fx.ParamTags(`group:"routes"`),
 	),
 )
+
+func newHttpServerFactory(addr string) func(fx.Lifecycle, *http.ServeMux, *log.Logger) *http.Server {
+	return func(lc fx.Lifecycle, mux *http.ServeMux, logger *log.Logger) *http.Server {
+		s := &http.Server{Addr: addr, Handler: mux}
+
+		lc.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				ln, err := net.Listen("tcp", s.Addr)
+				if err != nil {
+					return err
+				}
+
+				go s.Serve(ln)
+				logger.Info("started http server", "addr", s.Addr)
+
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				return s.Shutdown(ctx)
+			},
+		})
+
+		return s
+	}
+}
 
 func newHttpServer(lc fx.Lifecycle, mux *http.ServeMux, logger *log.Logger) *http.Server {
 	s := &http.Server{Addr: ":8080", Handler: mux}
